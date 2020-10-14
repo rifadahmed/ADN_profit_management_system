5409404f3b9388c5d4e2a83d023e486d583fb44c
Jobayer Al Mahmud Tuser <jtuser112@gmail.com>
2020-10-14 16:18:16 +0600

    footer changed

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,3 @@
  <footer class="main-footer">
-    <strong>Copyright &copy; <?php echo date('Y') ?> Made By <a href="https://adndigital.com.bd/">ADN Digital</a></strong>
-    Design By
-    <span class="text-right">
-         <strong>Tuser, Rifad </strong>
-    </span>
+    <strong>Copyright &copy; <?php echo date('Y') ?> Developed By <a href="https://adndigital.com.bd/">ADN Digital</a></strong>
  </footer>
